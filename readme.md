ac26714ea0ded7cb21821982cea0da847d7032e5
pz <pazhersh@gmail.com>
2023-05-06 22:10:13 +0300

    added basic functionality (ex 2)

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Corsight.ai home assignment
+by paz hershberg
+
+## Forward
+
+I would like to start by apoligizing - I am aware that this is meant to be ran on a linux machine (specifically ubuntu), but since I only own windows machines I never tested it on a linux one.
+
+That said, I hope that most commands will work on both operating systems, as python should make the overall environment irrelevant for most of its usage.
---
+++ src/os_operations.py
@@ -0,0 +1,18 @@
+import subprocess
+import os
+
+class OsOperations():
+    def read_disk_stats(self, disk_path):
+        raise('not implemented yet')
+
+        stats = statvfs = os.statvfs(disk_path)
+        return stats
+    
+    def set_env_var(self, key, value):
+        if os.name == 'posix':  # if is in linux
+            exp = f'export {key}="{value}"' # TODO: test via docker or something
+        if os.name == 'nt':  # if is in windows
+            exp = f'setx {key} "{value}"'
+        subprocess.Popen(exp, shell=True).wait()
+    
+OsOperations().set_env_var('paztest', '1')
